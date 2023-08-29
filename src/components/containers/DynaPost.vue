d0732abeb33dd5f276725d953e94c7607505ac8b
Thertzlor <timo.hertzler@gmail.com>
2023-08-29 16:24:12 +0200

    rounding

--- src/components/containers/DynaPost.vue
+++ src/components/containers/DynaPost.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import {computed} from 'vue';
+import {useGlobals} from '@/stores/globals';
 import CustomLink from '../navigation/CustomLink.vue';
 import ImageContainer from '../containers/ImageContainer.vue';
 import type {UploadFileEntity} from '@/graphql/api';
 const props = defineProps<{content:string, imgs:SomeOf<UploadFileEntity>[]}>();
-
+const {rounder} = useGlobals();
 const findSource = imgString => (/src="([^"]+)"/.exec(imgString) || [])[1] || '';
 
 const imageTransform = (imageStr:string) => {
@@ -12,7 +13,7 @@ const imageTransform = (imageStr:string) => {
   const file = props.imgs.map(f => f?.attributes).filter((f):f is NonNullable<typeof f> => !!f).find(({url}) => url === src);
   if (!file) return imageStr;
   const percentage = /style="width:([\d.]+)/.exec(imageStr)?.[1];
-  const multiplier = parseFloat(percentage || '25') / 100;
+  const multiplier = rounder(parseFloat(percentage || '25') / 100);
   const blank = src.endsWith('png');
   const [l, r] = ['left', 'right'].map(t => imageStr.includes(`image-style-align-${t}`));
   const classText = `image${l ? ' style-align-left' : ''}${r ? ' style-align-right' : ''}${blank ? ' blank' : ''}`;
