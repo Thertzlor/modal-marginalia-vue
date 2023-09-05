ee631ebeda603fec91583587277e50a37caa55b6
Thertzlor <timo.hertzler@gmail.com>
2023-09-05 16:18:08 +0200

    all highlighint server side

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,7 @@ import {DefaultApolloClient} from '@vue/apollo-composable';
 import {ApolloClient, InMemoryCache, HttpLink, from} from '@apollo/client/core';
 import {onError} from '@apollo/client/link/error';
 import {useGlobals} from '@/stores/globals';
-import {useHighlighting} from '@/stores/highlighting';
-import hljs from 'highlight.js';
 import 'highlight.js/styles/monokai.css';
-import javascript from 'highlight.js/lib/languages/javascript';
 
 declare module '@vue/runtime-core' {
   interface ComponentCustomProperties extends ReturnType<typeof useGlobals> {}
@@ -22,11 +19,8 @@ const errorLink = onError(({graphQLErrors, networkError}) => {
 });
 
 const pinia = createPinia();
-const app = createApp({render: () => h(App),compilerOptions:{isCustomElement(t){return ['CustomLink'].includes(t);}}}).use({install: (a, o:{hljs:typeof hljs}) => a.directive('highlight', (el:HTMLPreElement) => el.querySelectorAll<HTMLElement>('code').forEach(o.hljs.highlightElement))}, {hljs}).use(router).use(pinia);
+const app = createApp({render: () => h(App),compilerOptions:{isCustomElement(t){return ['CustomLink'].includes(t);}}}).use(router).use(pinia);
 //Pina is loaded now
-const highlight = useHighlighting();
-hljs.registerLanguage('javascript',javascript);
-hljs.addPlugin(highlight.linePlugin);
 
 const globals = useGlobals();
 app.config.globalProperties = globals as any;
