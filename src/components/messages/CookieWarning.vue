d87cf27348aee752f52777188752f07ee7c37ff8
Thertzlor <timo.hertzler@gmail.com>
2023-09-07 12:47:49 +0200

    cookie consent

---
+++ src/components/messages/CookieWarning.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+defineProps<{visible:boolean}>();
+const emit = defineEmits<{(e:'confirm',arg:boolean)}>();
+</script>
+
+<template>
+  <div v-if="visible" id="cookie-warning">In order to save your style preset, I'll have to save a cookie, is that okay?<button @click="emit('confirm',true)">YES</button> <button @click="emit('confirm',false)">NO</button></div>
+</template>
