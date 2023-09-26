f9e97ffbdc72cee8fa0c7f81ba16fd5058632f92
Thertzlor <timo.hertzler@gmail.com>
2023-09-26 13:00:37 +0200

    final cookie styling

--- src/components/messages/CookieWarning.vue
+++ src/components/messages/CookieWarning.vue
@@ -11,6 +11,8 @@ const emi = (b:boolean) => ((decision.value = b),setTimeout(() => emit('confirm'
     <span v-if="decision">Spoken like a true warrior.<br>Now go in peace.</span>
     <span v-else-if="decision===false">That's understandable.<br>One can never be too careful these days.</span>
     <span v-else>To save your style preset this site needs to save a <strong>cookie</strong> on this device.<br>Do you consent?</span>
-    <template v-if="decision == undefined"><br><button v-for="n of ['Yes','No']" :key="n" @click="emi(n=='Yes')">{{ n }}</button></template>
+    <template v-if="decision == undefined"><br><button
+      v-for="n of ['Yes','No']" :key="n" class="grayborder hoverglow"
+      @click="emi(n=='Yes')">{{ n }}</button></template>
   </div>
 </template>
