f4a96193d956555ac8ea62de04beb7160aed5f22
Thertzlor <timo.hertzler@gmail.com>
2024-01-14 16:09:02 +0100

    adding generic message

---
+++ src/components/messages/GenericMessage.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+defineProps<{msg:string}>();
+const emit = defineEmits<{(e:'confirm',arg:boolean)}>();
+const emi = (b:boolean) => emit('confirm',b);
+</script>
+
+<template>
+  <div class="grayborder modal-message">
+    <span>{{ msg }}</span>
+    <br><button class="grayborder hoverglow" @click="emi(true)">Ok</button>
+  </div>
+</template>
