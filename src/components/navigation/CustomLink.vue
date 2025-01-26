8be4495ef3d983379a168d7e26f76d8e742e9700
Thertzlor <timo.hertzler@gmail.com>
2025-01-26 20:22:20 +0100

    fixed custom link props

--- src/components/navigation/CustomLink.vue
+++ src/components/navigation/CustomLink.vue
@@ -1,29 +1,27 @@
 <script setup lang="ts">
-import {computed} from 'vue';
+import {computed, Prop} from 'vue';
 import {RouterLink} from 'vue-router';
 import {useGlobals} from '@/stores/globals';
 
 const {scrollOption,unHash} = useGlobals();
-const props = defineProps({...(RouterLink as any).props, noBlank: Boolean});
-const emit = defineEmits<{(arg:'nav')}>();
+const props = defineProps({...(RouterLink as any as {props:{to:Prop<string>}}).props, noBlank: Boolean});
+const emit = defineEmits<{(arg:'nav'),(arg)}>();
 const isExt = computed(() => typeof props.to === 'string' && props.to.startsWith('http'));
-const isAnchor = computed(() => !isExt.value && props.to.startsWith('#'));
+const isAnchor = computed(() => !isExt.value && props.to!.startsWith('#'));
 const scrolly = (anch:string) => ((d=document.getElementById(anch.slice(1))) => (d && (d?.scrollIntoView(scrollOption),d.focus(), history.pushState({}, '',`${unHash(window.location)}#${d?.id??''}`) ,emit('nav'))))();
 const hl = (e:Event) => (e.target as HTMLElement).classList.add('highlight');
 const dl = (e:Event) => (e.target as HTMLElement).classList.remove('highlight');
 </script>
 
 <template>
-  <!-- @vue-expect-error hacky templates -->
   <a
     v-if="isExt" :target="noBlank ? 'self' : '_blank'" :href="to"
     @focus="hl" @blur="dl"><slot /></a>
-  <!-- @vue-expect-error more hacky templates -->
   <a
     v-else-if="isAnchor" :href="to" @focus="hl"
-    @blur="dl" @click.stop.prevent="() => scrolly(to)"><slot /></a>
-  <!-- @vue-expect-error even more hacky templates -->
+    @blur="dl" @click.stop.prevent="() => scrolly(to!)"><slot /></a>
   <RouterLink
-    v-else v-bind="props" @focus="hl"
+    v-else v-bind="props" :to="to!"
+    @focus="hl"
     @blur="dl"><slot /></RouterLink>
 </template>
