4f9c925b36f4980f34f180fc653cec9515eea39c
Thertzlor <timo.hertzler@gmail.com>
2025-02-03 11:00:12 +0100

    better alt text handling

--- src/components/containers/ImageContainer.vue
+++ src/components/containers/ImageContainer.vue
@@ -3,7 +3,7 @@ import {computed, ref} from 'vue';
 import {useGlobals} from '@/stores/globals';
 
 const {getImageData, getSrcSet,getImageFile} = useGlobals();
-const props = defineProps<{ className:string, customSize?:string, image?:string, caption?:string}>();
+const props = defineProps<{ className:string, customSize?:string, image?:string, caption?:string, alt?:string}>();
 // const data = computed(() => (typeof props.imgData === 'string' ? JSON.parse(props.imgData) as UploadFile : props.imgData));
 const data = computed(() => getImageFile(props.image));
 const iData = computed (() => data.value && getImageData(data.value));
@@ -16,7 +16,7 @@ const iml = ref(false);
   <figure v-if="data && iData" :class="className" class="invisible">
     <a :href="image">
       <img
-        :srcset="getSrcSet(iData)" :src="data.url" :alt="data.alternativeText??undefined"
+        :srcset="getSrcSet(iData)" :src="data.url" :alt="(alt||data.alternativeText)??undefined"
         :style="{aspectRatio: `${data.width} / ${data.height}`, width: iml ? 'auto' : '', maxHeight:className.includes('banner')?undefined:`${data.height}px`}" :sizes="customSize || sizeText" :height="data.height??undefined"
         :width="data.width??undefined" @load="e => ((iml = true), imgload(e, 2))">
     </a>
